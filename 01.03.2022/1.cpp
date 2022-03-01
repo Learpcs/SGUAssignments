31f49ebd64453d65fe0a364187cdaabcd56a34db
Fanarill <learpcs@gmail.com>
2022-03-01 22:01:34 +0300

    1.cpp added

--- 01.03.2022/1.cpp
+++ 01.03.2022/1.cpp
@@ -1,2 +1,39 @@
 #include <iostream>
+using namespace std;
 
+int N;
+
+void line(int n, int character)
+{
+	if(n > 0)
+	{
+		cout << character;
+		line(n - 1, character);
+	}
+	else if(n == 0)
+	{
+		cout << '\n';
+	}
+}
+
+int f(int n, int character)
+{
+	if(n < N)
+	{
+		line(n + 1, character);
+		f(n + 1, !character);
+		if(n + 1 != N)
+			line(n + 1, character);
+	}
+}
+
+int rec(int n)
+{
+	f(0, 0);
+}
+
+int main()
+{
+	cin >> N;
+	rec(N);
+}
